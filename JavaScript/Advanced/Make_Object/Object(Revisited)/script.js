0e09f926b594582d222156b260c2d9ad12892bd9
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-07-23 08:08:51 +0700

    Mengedit sedikit terkait Object.Create()

--- JavaScript/Advanced/Make_Object/Object(Revisited)/script.js
+++ JavaScript/Advanced/Make_Object/Object(Revisited)/script.js
@@ -19,38 +19,38 @@ let energiMurid = 'dasar lemah';
 
 // kekurangannya ada di bawah ini karena ketika fungsi ini di panggil maka fungsi yang ke bawah juga ikut diduplikat padahal parameternya cuman ada dua. jadi tidak efektif
 
-  // murid.makan = function (porsi) {
-  //   this.energi += porsi;
-  //   console.log(`Selamat datang ${this.nama}, Kamu makan ${energi} porsi`);
-  // };
+// murid.makan = function (porsi) {
+//   this.energi += porsi;
+//   console.log(`Selamat datang ${this.nama}, Kamu makan ${energi} porsi`);
+// };
 //   return murid;
 // }
-  /** 
-   * * solusinya ialah di bawah ini
-  */
- 
-  const methodMurid = {
-    makan: function(porsi) {
-      this.energi += porsi;
-      console.log(`Selamat datang ${this.nama}, Kamu makan ${porsi} porsi`);
-    },
-    main: function(jam) {
-      this.energi -= jam;
-      console.log(`Halo ${this.nama}, Kamu main ${jam} jam`)
-    },
-    tidur: function(jam) {
-      this.energi += jam;
-      console.log(`Halo ${this.nama}, Kamu sudah tidur ${jam} jam`)
-    }
-  }
+/**
+ * * solusinya ialah di bawah ini
+ */
 
-   function pendidikan(nama, energi) {
-    let murid = Object.create(methodMurid); // perbedaan method ini dengan cara sebelumnya ialah kita bisa memberikan parameter di Object.create() yang mengacu pada element parentnya sehingga dapat membawa property dari object lainnya
-    murid.nama = nama;
-    murid.energi = energi;
+const methodMurid = {
+  makan: function (porsi) {
+    this.energi += porsi;
+    console.log(`Selamat datang ${this.nama}, Kamu makan ${porsi} porsi`);
+  },
+  main: function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, Kamu main ${jam} jam`);
+  },
+  tidur: function (jam) {
+    this.energi += jam;
+    console.log(`Halo ${this.nama}, Kamu sudah tidur ${jam} jam`);
+  },
+};
 
-    return murid;
-   }
+function pendidikan(nama, energi) {
+  let murid = Object.create(methodMurid); // perbedaan method ini dengan cara sebelumnya ialah kita bisa memberikan parameter di Object.create() yang mengacu pada element parentnya sehingga dapat membawa property dari object lainnya
+  murid.nama = nama;
+  murid.energi = energi;
+
+  return murid;
+}
 let dataIsmail = pendidikan('Ismail', 98);
 let dataDoddy = pendidikan('Doddy', 88);
 
@@ -59,7 +59,6 @@ let dataDoddy = pendidikan('Doddy', 88);
  * * Karena bila ada object baru di variable method Murid maka kita harus ikut menambahkan method tersebut.
  * * Ada cara yan lebih efectife lagi yatitu menggunakan object.create()
  */
-
 
 // 3. constructor function
 /**
@@ -78,5 +77,3 @@ function Pendidikan(nama, energi) {
 }
 
 var dataMurid = new Pendidikan('ismail', 98);
-
-// Object.create
