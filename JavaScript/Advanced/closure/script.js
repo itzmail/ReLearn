312f58dddb1e678e5e9e20eb8ac35a8d11a0b03e
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-07-24 19:54:27 +0700

    belajar closure

---
+++ JavaScript/Advanced/closure/script.js
@@ -0,0 +1,60 @@
+// Closure
+/**
+ * * Closure adalah sebuah function yang memiliki akses terhadap parent scope
+ * * Closure berguna untuk:
+ * ? membuat function factories
+ * ? membuat private method
+let nama = 'Ismail';
+function init() {
+  return function tampilNama(nama) {
+    console.log(nama);
+  };
+}
+
+let panggilNama = init();
+panggilNama('ismail');
+panggilNama('alam');
+*/
+
+// function factory
+function ucapkanSalam(waktu) {
+  return function (nama) {
+    return `Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan`;
+  };
+}
+
+// ucapkanSalam('Malam')('ada'); // Begitu bisa tetapi tidak dicontohkan
+let selamatPagi = ucapkanSalam('Pagi');
+let selamatSiang = ucapkanSalam('Siang');
+let selamatMalam = ucapkanSalam('Malam');
+
+console.log(selamatMalam('Ismail'));
+
+// private methode atau private variable
+
+// const add = () => {
+//   let counter = 0;
+//   return function () {
+//     return ++counter;
+//   };
+// };
+
+// let a = add();
+
+// console.log(a());
+// console.log(a());
+// console.log(a());
+
+// Immedietly invoke method
+const add = (function () {
+  let counter = 0;
+  return function () {
+    return ++counter;
+  };
+})();
+
+// let a = add();
+
+console.log(add());
+console.log(add());
+console.log(add());
