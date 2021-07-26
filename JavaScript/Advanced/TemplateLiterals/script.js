558b109f01a756b6ff21085cd66d9876a27b8b7b
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-07-26 19:19:59 +0700

    Belajar Literal Template

---
+++ JavaScript/Advanced/TemplateLiterals/script.js
@@ -0,0 +1,24 @@
+const user = 'Ismail';
+// Embeded Expressions
+console.log(`Hasil dari 2 x 4 x 8 = ${2 * 4 * 8}`);
+// console.log(alert('halo'));
+
+// HTML Fragments
+console.log(`<div class="htmlFragments">Halo ${user}</div>
+    <h2>Apa Kabar ${user}</h2>    
+`);
+
+const santri = [
+  {
+    nama: 'Ismail Nur Alam',
+    email: 'ismailnuralam@imaco.com',
+  },
+  {
+    nama: 'Chila on Seven',
+    email: 'Kucing Lucu',
+  },
+  {
+    nama: 'ManMan',
+    email: 'manjaro@manjaro.com',
+  },
+];
