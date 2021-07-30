c5af3a0a413028447c37adcb2e2a290f07a869e2
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-07-30 08:48:49 +0700

    Belajar Asynchronous

--- JavaScript/Advanced/Asynchronous/script.js
+++ JavaScript/Advanced/Asynchronous/script.js
@@ -13,7 +13,7 @@
  * * non-blocking
  * * asynchronous and
  * * concurrent language
- * 
+ *
  * * Single Thread
  * "Urutan eksekusi kode yang dapat dilakukan secara bebas/independent satu sama lain". jadi dikerjakan satu persatu secara urut
  * * Non-Blocking
@@ -22,10 +22,18 @@
  *  lihat video ini https://youtu.be/wVtdv5BHhno?list=PLFIM0718LjIUGpY8wmE41W7rTJo_3Y46-&t=203 (Video dari pak sandhika)
  * ? asynchronous + single threade = concurency
  * ? asynchronous + multi threaded = parallelism
- * 
+ *
  * TODO kesimpulan
  * Single or multi, intinya adalah tergangtung Lingkunan eksekusi 'task'
  * blocking or non-block, tergantung dari teknik 'ngoding (IO related)
  * synchronous or asynchronous,  tergantung dari teknik 'ngoding' (HTTP Reques realated)
  * concurrent or parallel, tergantung Lingkungan eksekusi 'task'
+ */
+
+/**
+ * TODO Let's Dig Deeper
+ * * JS punya V8 engine
+ * penjelasan V8 Engine version JS look this video https://youtu.be/wVtdv5BHhno?list=PLFIM0718LjIUGpY8wmE41W7rTJo_3Y46-&t=370
+ * * Simulation event loop
+ * latenflip.com/loupe
  */
