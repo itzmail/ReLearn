1b3277f20b77c77450b38d274d08c6fe84f330cf
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-08-02 11:16:52 +0700

    Menyempurnakan Web Movies

--- JavaScript/Advanced/Asynchronous/callback(latihan)/script.js
+++ JavaScript/Advanced/Asynchronous/callback(latihan)/script.js
@@ -1,65 +1,78 @@
-$.ajax({
-  url: 'http://www.omdbapi.com/?apikey=4c1ce192&s=avengers',
-  success: (results) => {
-    const movies = results.Search;
-    console.log(movies);
-    let cards = '';
-    movies.map((m) => {
-      console.log(m);
-      cards += `<div class="col-md-4 my-3">
-        <div class="card">
-            <img src="${m.Poster}" class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">${m.Title}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-              <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
-            </div>
-          </div>
-    </div>`;
-    });
-    $('.movie-container').html(cards);
-
-    // ketika tombol di-klik
-    $('.modal-detail-button').on('click', function () {
-      $.ajax({
-        url:
-          'http://www.omdbapi.com/?apikey=4c1ce192&i' + $(this).data('imdbid'),
-        success: (m) => {
-          const movieDetail = `<div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img src="${m.Poster}" alt="" class="img-fluid">
-                    </div>
-                    <div class="col-md">
-                      <ul class="list-group">
-                          <li class="list-group-item">
-                            <h4>${m.Title} (${m.Year})</h4>
-                          </li>
-                          <li class="list-group-item">
-                            <strong>Director :</strong> ${m.Director}
-                          </li>
-                          <li class="list-group-item">
-                            <strong>Actors :</strong> ${m.Actors}
-                          </li>
-                          <li class="list-group-item">
-                            <strong>Writers :</strong> ${m.Writer}
-                          </li>
-                          <li class="list-group-item">
-                            <strong>Plot :</strong> ${m.Plot}
-                          </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>`;
-          $('.modal-body').html(movieDetail);
-        },
-        error: (e) => {
-          console.log(e.responseText);
-        },
+$('.search-button').on('click', function () {
+  $.ajax({
+    url:
+      'http://www.omdbapi.com/?apikey=4c1ce192&s=' + $('.input-keyword').val(),
+    success: (results) => {
+      const movies = results.Search;
+      // console.log(movies);
+      let cards = '';
+      movies.forEach((detail) => {
+        // console.log(m);
+        cards += showCards(detail);
       });
-    });
-  },
-  error: (e) => {
-    console.log(e.responseText);
-  },
+      $('.movie-container').html(cards);
+
+      // ketika tombol di-klik
+      $('.modal-detail-button').on('click', function () {
+        // console.log($(this).data('imdbid'));
+        $.ajax({
+          url:
+            'http://www.omdbapi.com/?apikey=4c1ce192&i=' +
+            $(this).data('imdbid'),
+          success: (m) => {
+            const movieDetail = showMovieDetail(m);
+            $('.modal-body').html(movieDetail);
+          },
+          error: (e) => {
+            console.log(e.responseText);
+          },
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
 });
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+  <div class="card">
+      <img src="${m.Poster}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${m.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+      </div>
+    </div>
+</div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+  <div class="row">
+      <div class="col-md-3">
+          <img src="${m.Poster}" alt="" class="img-fluid">
+      </div>
+      <div class="col-md">
+        <ul class="list-group">
+            <li class="list-group-item">
+              <h4>${m.Title} (${m.Year})</h4>
+            </li>
+            <li class="list-group-item">
+              <strong>Director :</strong> ${m.Director}
+            </li>
+            <li class="list-group-item">
+              <strong>Actors :</strong> ${m.Actors}
+            </li>
+            <li class="list-group-item">
+              <strong>Writers :</strong> ${m.Writer}
+            </li>
+            <li class="list-group-item">
+              <strong>Plot :</strong> ${m.Plot}
+            </li>
+          </ul>
+      </div>
+  </div>
+</div>`;
+}
