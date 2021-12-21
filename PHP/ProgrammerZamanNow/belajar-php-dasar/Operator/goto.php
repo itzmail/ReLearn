1f44f3d5bc6cd41e232caeaee2ee0034b3a11c07
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-12-21 13:11:12 +0700

    Belajar goto dan juga function

--- PHP/ProgrammerZamanNow/belajar-php-dasar/Operator/goto.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/Operator/goto.php
@@ -14,3 +14,16 @@ echo "Lokasi blok B \n";
 
 a:
 echo "Anda sudah sampai di lokasi blok A" . PHP_EOL;
+
+/* Operator GoTo dipakai untuk  */
+$counter = 1;
+while(true) {
+    echo "Looping ke-$counter" . PHP_EOL;
+    $counter++;
+    if($counter >= 10) {
+        goto end;
+    }
+}
+
+end :
+echo "Looping selesai";
