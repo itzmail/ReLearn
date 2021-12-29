fe8c622ab7615cce93125d755671e769a8c093e0
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-12-29 22:19:07 +0700

    Belajar string function untuk memodifikasi data string

---
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/function/array_function.php
@@ -0,0 +1,5 @@
+<?php
+/*
+* sama halnya dengan string function array function digunakan untuk memanipulasi data array
+*
+* bisa cek di link ini https://www.php.net/manual/en/ref.array.php*/
---
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/function/string_function.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* String function adalah function bawaan dari php yang berguna untuk memodifikasikan String
+* 
+* karena banyak bisa cek di link ini : https://www.php.net/manual/en/ref.strings.php*/
+
+// contoh
+var_dump(",", [1, 2, 'ok', 4, 5]);
+var_dump(explode(" ", 'Ismail Nur Alam'));
+var_dump(trim("    Ismail     "));
+var_dump(substr("Ismail Nur Alam", 0, 10));
