ddbd5f79e5888a63f6a3dafe9cb7501167053499
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-12-30 11:19:55 +0700

    belajar array, string, dan is function

--- PHP/ProgrammerZamanNow/belajar-php-dasar/function/string_function.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/function/string_function.php
@@ -5,7 +5,8 @@
 * karena banyak bisa cek di link ini : https://www.php.net/manual/en/ref.strings.php*/
 
 // contoh
-var_dump(",", [1, 2, 'ok', 4, 5]);
+echo "join";
+var_dump(join(",", [1, 2, 3, 4, 5]));
 var_dump(explode(" ", 'Ismail Nur Alam'));
 var_dump(trim("    Ismail     "));
 var_dump(substr("Ismail Nur Alam", 0, 10));
