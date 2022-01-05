b77517b86bc1b46b709dd424de435436dda73a14
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-05 18:02:08 +0700

    Belajar namespace untuk function dan constanta

---
+++ PHP/ProgrammerZamanNow/OOP/namespace/funConst.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HELPER;
+
+function helpMe()
+{
+  echo "HELP ME" . PHP_EOL;
+}
+
+const APPLICATION = "Belajar PHP OOP";
---
+++ PHP/ProgrammerZamanNow/OOP/namespace/view_funConst.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "./funConst.php";
+
+echo HELPER\APPLICATION . PHP_EOL;
+
+HELPER\helpMe();
