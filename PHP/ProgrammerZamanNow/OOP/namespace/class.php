ea5503cedef72902b7e855cb4140d3a057d36730
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-06 21:58:24 +0700

    Belajar alias namespace

--- PHP/ProgrammerZamanNow/OOP/namespace/class.php
+++ PHP/ProgrammerZamanNow/OOP/namespace/class.php
@@ -1,8 +1,14 @@
 <?php
-namespace Eksekutif;
-
-class OOP {
-  var string $materi = "Namespace";
-  var $hobby = ["Baca", "Coding", "Game"];
+namespace Eksekutif {
+  class OOP {
+    var string $materi = "Namespace";
+    var $hobby = ["Baca", "Coding", "Game"];
+  }
 }
 
+namespace Data {
+  class OOP {
+    var string $materi = "Alias";
+    var $member = ['Ismail', 'Yoga', 'Janto'];
+  }
+}
