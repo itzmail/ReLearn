a6158889291d49fe76a8938c25709386298beb6f
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-06 14:18:21 +0700

    Belajar alias dalam namespace 1 jam 8 menit

---
+++ PHP/ProgrammerZamanNow/OOP/namespace/alias.php
@@ -0,0 +1,5 @@
+<?php
+/*Saat kita menggunakan use kita tidak perlu menuliskan namespacenya
+* Tapi yang jadi masalaha adalah ketika kita menuliskan nama class misalnya, nama tersebut sama maka akan terjadi conflict
+* Maka dari itu kita bisa menggunakan salah satu fitur dari PHP yaitu alias
+* Caranya dengan menggunakan as sebagai keyword setelah melakukan use*/
