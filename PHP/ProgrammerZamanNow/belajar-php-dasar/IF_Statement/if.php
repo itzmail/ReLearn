bc0af82fc4f6fcf45183eeb7795e6962c227ea85
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-10 07:20:04 +0700

    Tambah Keterangan

--- PHP/ProgrammerZamanNow/belajar-php-dasar/IF_Statement/if.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/IF_Statement/if.php
@@ -1,11 +1,13 @@
 <?php
 $nilai = 90;
 $total_murid = 100;
-$ACCEPT = 'Selamat anda diterima';
-$DECLINE = 'Maaf anda belum berhasil';
+
+// dua macam penulisan constant
+define('ACCEPT', 'Selamat kalian lulus');
+const DECLINE = 'Maaf anda belum berhasil';
 
 if($nilai >= 75 && $total_murid >= 50) {
-    echo "$ACCEPT";
+    echo ACCEPT;
 } else {
     echo $DECLINE;
 }
