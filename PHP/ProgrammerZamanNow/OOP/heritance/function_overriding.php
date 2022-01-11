fa260edaf856249df934369862bdcc6b62b3630a
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-11 20:22:12 +0700

    Belajar Constructor overriding

--- PHP/ProgrammerZamanNow/OOP/heritance/function_overriding.php
+++ PHP/ProgrammerZamanNow/OOP/heritance/function_overriding.php
@@ -7,25 +7,39 @@
 
 class Manager {
      var string $name;
+     var string $title;
+
+     // Materi constructor overriding
+  function __construct(string $name = "", string $title = "Manager"){
+    $this->name = $name;
+    $this->title = $title;
+  }
 
   function greetings(?string $name) : void    
   {  
-        echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
+        echo "Hi $name, my name is $this->title $this->name" . PHP_EOL;
 
   }
 }
 
 class VicePresident extends Manager{
+
+  // Constructor yang di-overriding
+  function __construct(string $name ="") {
+    // tidak wajib untuk mengambil dari parent tapi direkomendasikan
+    parent::__construct($name, "VP");
+  }
+
   function greetings(?string $name) : void // function ini ditimpa dari function parentnya 
     {  
-        echo "Hi $name, my name is VP $this->name" . PHP_EOL;
+        echo "Hi $name, my name is $this->title $this->name" . PHP_EOL;
     }
 }
 
-$manager = new Manager();
-$manager->name="Ismail";
-$manager->greetings("Joko"); // yang akan dijalankan function milik Manager
+// $manager = new Manager();
+// $manager->name="Ismail";
+// $manager->greetings("Joko"); // yang akan dijalankan function milik Manager
 
-$vp = new VicePresident();
-$vp->name = "Alam";
-$vp->greetings("Joko"); // yang akan dijalankan function milik Vice President bukan milik parentnya(Manager)
+// $vp = new VicePresident();
+// $vp->name = "Alam";
+// $vp->greetings("Joko"); // yang akan dijalankan function milik Vice President bukan milik parentnya(Manager)
