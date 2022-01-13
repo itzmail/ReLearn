102680b99ac6e26b85663727b6780dca9887d700
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-01-13 17:45:12 +0700

    Belajar getter and setter 2:0

---
+++ PHP/ProgrammerZamanNow/OOP/getter_setter/index.php
@@ -0,0 +1,29 @@
+<?php
+
+class Category{
+ private string $name;
+ private bool $expensive;
+
+  function getName() : string {
+    return $this->name;
+  }
+
+  function setName(?string $input) : void {
+    $this->name = $input;
+  }
+
+  function isExpensive() : bool {
+    return $this->expensive;
+  }
+
+  function setExpensive(bool $input) : void {
+    $this->expensive = $input;
+  }
+}
+
+$cg = new Category();
+$cg->setName('MSI');
+$cg->setExpensive(true);
+
+echo "Nama barang : {$cg->getName()}" . PHP_EOL;
+echo "Apakah produk tersebut mahal {$cg->isExpensive()}" . PHP_EOL;
