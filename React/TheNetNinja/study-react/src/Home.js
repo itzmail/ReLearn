93cc12a4e0245c075bb81818ce5dd8481b13fa76
ismailify <ismailnuralam@gmail.com>
2022-02-09 15:22:55 +0700

    Belajar Even

--- React/TheNetNinja/study-react/src/Home.js
+++ React/TheNetNinja/study-react/src/Home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('Ismail');
+    const [age, setAge] = useState(21)
+
+    const clickMe = (event) => {
+        console.log('Hey you there' + name)
+        console.log('THis is your age' + age)
+    }
+
     return(
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={clickMe}>Click Me!</button>
         </div>
     )
 }
