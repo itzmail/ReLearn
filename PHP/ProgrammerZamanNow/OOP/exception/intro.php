42bf5b28eb4a620742548a8b9422d4363b1fea24
ismailify <ismailnuralam@gmail.com>
2022-02-15 05:47:11 +0700

    Belajar Exception

---
+++ PHP/ProgrammerZamanNow/OOP/exception/intro.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Saat kita membuat aplikasi, ktia tidak akan terhindar dengan yang namanya error
+ * Di PHP, error direpresentasikan dengan istilah exception, dan semua direpresentasikan dalam bentuk class exception
+ * Kita bisa menggunakan class exception sendiri, atau menggunakan yang sudah disediakan oleh PHP
+ * Jika kita ingin membuat exception, maka kita harus membuat class yang implement interface Throwable atau turunan-turunannya
+ */
+
+ class ValidationException extends Exception {
+
+ }
+
+ /**
+  * Membuat Exception
+  * Exception biasanya terjadi difunction
+  * Di dalam kode program kita, untuk membuat exception kita cukup menggunakan kata kunci throw, diikuti dengan object exception nya
+  */
+
+  class LoginRequest {
+      public string $username;
+      public string $password;
+  }
+
+function validateLoginRequest(LoginRequest $request)
+{
+    if(!isset($request->username)) {
+        throw new ValidationException("Username is null");
+    } else if(!isset($request->password)) {
+        throw new ValidationException("Password is null");
+    } else if(trim($request->username) == "") {
+        throw new Exception("Username is empty");
+    } else if(trim($request->password) == "") {
+        throw new Exception("Password is empty");
+    } else {
+        echo "Data valid" . PHP_EOL;
+    }
+}
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "eko";
+// $loginRequest->password = "";
+
+validateLoginRequest($loginRequest);
