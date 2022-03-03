023fc6d1fdaa541112293b6d579dc28a940a96a9
ismailify <ismailnuralam@gmail.com>
2022-03-03 19:23:37 +0700

    Belajar Tipe data char dan Tipe Data Bool

---
+++ Java/Java_Basic/src/TipeDataBool.java
@@ -0,0 +1,12 @@
+public class TipeDataBool {
+
+    public static void main(String[] args) {
+        /*Default dari tipe data boolean adalah false*/
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+    }
+
+}
