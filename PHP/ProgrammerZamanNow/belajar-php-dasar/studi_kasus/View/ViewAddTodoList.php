211d8be00433075e7e82a1fe27d325c256b8b0c8
ismailify <ismailnuralam@gmail.com>
2022-03-03 09:18:43 +0700

    Berhasil Membuat Sistem View add dan show todolist

--- PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewAddTodoList.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewAddTodoList.php
@@ -1,6 +1,20 @@
 <?php
 
+require_once "../BussinessLogic/AddTodoList.php";
+require_once "../Model/TodoList.php";
+require_once "../Helper/Input.php";
+
 function ViewAddTodoList()
 {
+    echo "Menambah TODO" . PHP_EOL;
+    
+    $todo = input("Todo (0 untuk batal)") . PHP_EOL;
+
+
+    if($todo != 0) {
+        addTodoList($todo);
+    } else {
+        echo "Kamu Batal Menambah Todo List" . PHP_EOL;
+    }
     
 }
--- PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewShowTodoList.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewShowTodoList.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once "../Model/TodoList.php";
+require_once "../BussinessLogic/ShowTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function ViewShowTodoList() 
 {
+	while(true){
+	showTodoList();
 
+	echo "MENU" . PHP_EOL;
+	echo "1. Tambah Todo" . PHP_EOL;
+	echo "2. Hapus Todo" . PHP_EOL;
+	echo "x. Keluar" . PHP_EOL;
+
+	$pilihan = input("Pilih : ");
+
+	if($pilihan == "1") {
+		ViewAddTodoList();
+	} else if($pilihan == "2"){
+		ViewRemoveTodoList();
+	} else if($pilihan == "x"){
+		break;
+	} else {
+		echo "Pilihan tidak di mengerti" . PHP_EOL;
+	}
+	}
+
+	echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
