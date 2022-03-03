8386b48c1333d245684c526d59fecc9cc8066b53
ismailify <ismailnuralam@gmail.com>
2022-03-03 13:50:57 +0700

    Berhasil Menguji Applikasi

--- PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/VIewRemoveTodoList.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/VIewRemoveTodoList.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once "../Helper/Input.php";
-require_once "../BussinessLogic/RemoveTodoList.php";
+require_once __DIR__ . "/../Helper/Input.php";
+require_once __DIR__ . "/../BussinessLogic/RemoveTodoList.php";
 
 function ViewRemoveTodoList()
 {
--- PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewAddTodoList.php
+++ PHP/ProgrammerZamanNow/belajar-php-dasar/studi_kasus/View/ViewAddTodoList.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once "../BussinessLogic/AddTodoList.php";
-require_once "../Model/TodoList.php";
-require_once "../Helper/Input.php";
+require_once __DIR__ . "/../BussinessLogic/AddTodoList.php";
+require_once __DIR__ . "/../Model/TodoList.php";
+require_once __DIR__ . "/../Helper/Input.php";
 
 function ViewAddTodoList()
 {
     echo "Menambah TODO" . PHP_EOL;
     
-    $todo = input("Todo (0 untuk batal)") . PHP_EOL;
+    $todo = input("Todo (0 untuk batal)");
 
 
     if($todo != 0) {
