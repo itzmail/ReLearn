19f27db64a6ad4349e6bda53dc46a24dc84e422d
ismailify <ismailnuralam@gmail.com>
2022-03-23 16:10:03 +0700

    Make NavBar it's Not Completed

---
+++ React/react-responsive/src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavbarContainer, NavLogo, Nav, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements'
+import {FaBars} from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Ismail</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar
