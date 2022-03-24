af1d8c48f1b9453947060a51d0019614e2213e30
ismailify <ismailnuralam@gmail.com>
2022-03-24 19:56:47 +0700

    Belajar Membuat Method di Java(Mulai yang ada parameter hingga me return)

---
+++ Java/Java_Basic/src/MethodParameter.java
@@ -0,0 +1,9 @@
+public class MethodParameter {
+    public static void main(String[] args){
+        sayHello("Ismail", "Alam");
+    }
+
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
