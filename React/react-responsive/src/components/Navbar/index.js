95f59b08bbbf1a4fbf543fbb022f184f2e031a73
ismailify <ismailnuralam@gmail.com>
2022-03-25 13:21:48 +0700

    Menambahkan Sidebar

--- React/react-responsive/src/components/Navbar/index.js
+++ React/react-responsive/src/components/Navbar/index.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import {NavbarContainer, NavLogo, Nav, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements'
-import {FaBars} from 'react-icons/fa'
+import React from "react";
+import {
+  NavbarContainer,
+  NavLogo,
+  Nav,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to='/'>Ismail</NavLogo>
-                <MobileIcon>
-                    <FaBars />
-                </MobileIcon>
-                <NavMenu>
-                    <NavItem>
-                        <NavLinks to="about">About</NavLinks>
-                    </NavItem>
-                </NavMenu>
-            </NavbarContainer>
-        </Nav>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Ismail</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="sevices">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink>sign in</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ React/react-responsive/src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import {SidebarContainer, Icon, CloseIcon} from './SidebarElement';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+        <Icon>
+            <CloseIcon />
+        </Icon>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar;
