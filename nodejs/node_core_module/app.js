501b7edcc336b83291bd6f822df6f5975965993b
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-03-27 20:04:24 +0700

    Menyempurnakan Program Simple Add Contact

--- nodejs/node_core_module/app.js
+++ nodejs/node_core_module/app.js
@@ -1,28 +1,37 @@
 const fs = require('fs');
+const validator = require('validator');
 
 // console.log(fs);
 
-// Kita bisa menampung error, ini khusus untuk synchornous, untuk asynchronous error tersebut akan ditampung di callback
-try{
-  fs.writeFileSync("data/test.txt", "Hello World secara synchornous!");
-} catch(e){
-  console.log(e);
-}
-
 // Menuliskan secara asynchronous
-fs.writeFile('data/test.txt', 'Hello World Secara asynchronous.', e => console.log(e)); // bila terdapat file yang sama maka akan ditimpa isinya
+/* fs.writeFile('data/test.txt', 'Hello World Secara asynchronous.', e => console.log(e)); */ // bila terdapat file yang sama maka akan ditimpa isinya
 
 
 // Membaca isi file
 // synchornous
-const data = fs.readFileSync('data/test.txt', 'utf-8');
+/* const data = fs.readFileSync('data/test.txt', 'utf-8'); */
 // console.log(data);
 
 //asynchronous
-fs.readFile('data/test.txt', 'utf-8', (err, data) => {
+/* fs.readFile('data/test.txt', 'utf-8', (err, data) => {
   if(err) throw err;
   // console.log(data)
-})
+}) */
+
+/*
+  * Kita cek terlebih dahulu apakah file yang akan kita input ada tersebut
+  * Kita bisa menggunakan method existSync pada fs
+*/
+
+// Kita buat folder data bila tidak ada folder data
+if(!fs.existsSync('data')) {
+  fs.mkdirSync('data');
+}
+
+// Kita buat file contacts.json bila tidak file tersebut
+if(!fs.existsSync('data/contacts.json')){
+  fs.writeFileSync('data/contacts.json', '[]', 'utf-8');
+}
 
 /*
   * kita bisa memasukkan input di dalam terminal
@@ -34,21 +43,49 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const question1 = quesetion => {
+  return new Promise((resolve, reject) => {
+    rl.question("Siapa nama mu : ", (answer) => {
+      resolve(answer);
+    });    
+  })
+}
+
+const question2 = quesetion => {
+  return new Promise((resolve, reject) => {
+    rl.question("Email mu apa : ", (answer) => {
+      if(validator.isEmail('foo@bar.com')){
+        resolve(answer);
+      } else {
+        resolve(null);
+      }
+    });    
+  })
+}
+
+async function main() {
+  const nama = await question1();
+  const email = await question2();
+
+  const contact = {nama, email};
+  const file = fs.readFileSync("data/contacts.json", 'utf-8');
+  const contacts = JSON.parse(file);
+    
+  contacts.push(contact);
+    
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+  rl.close(); // harus diberi close() agar tidak terjadi hang.....
+}
+
+main();
+
 // studi kasus 
 /*
   1. Jadi pertama kita memasukkan input di terminal
   2. Lalu kita simpan input di terminal itu di file yang bernama contact.json
 */
-rl.question('Siapa namamu?', (nama) => {
-  // TODO: Log the answer in a database
-  rl.question('Nomor Telepon mu berapa?', (nomor) => {
-    const contact = {nama, nomor};
-    const file = fs.readFileSync("data/contacts.json", 'utf-8');
-    const contacts = JSON.parse(file);
-    
-    contacts.push(contact);
-    
-    fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-    rl.close(); // harus diberi close() agar tidak terjadi hang.....
-  })
-});
+// rl.question('Siapa namamu?', (nama) => {
+//   // TODO: Log the answer in a database
+//   rl.question('Nomor Telepon mu berapa?', (nomor) => {
+//   })
+// });
