227e3edd3c426b5a705530c90659af6642093211
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-03-28 20:50:04 +0700

    Belajar Compiling TypeScript

---
+++ typescript/compile_typescript/sandbox.js
@@ -0,0 +1,7 @@
+var hello = "Ismail";
+console.log(hello);
+var input = document.querySelectorAll('input');
+// console.log(input);
+input.forEach(function (input) {
+    console.log(input);
+});
---
+++ typescript/compile_typescript/sandbox.ts
@@ -0,0 +1,11 @@
+const hello = "Ismail";
+
+console.log(hello);
+
+const input = document.querySelectorAll('input');
+
+// console.log(input);
+
+input.forEach((input) => {
+    console.log(input);
+})
