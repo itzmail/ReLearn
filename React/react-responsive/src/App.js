5d38185db83753ca5732a92ba3e240eeccb795b6
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-04-04 10:40:34 +0700

    Just Push to Repo

--- React/react-responsive/src/App.js
+++ React/react-responsive/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Article, Brand, CTA, Featrue, Navbar } from './components';
 import { Blog, Feature, Footer, Header, Posibility, WhatGPT3 } from './containers';
+import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient_bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Featrue />
+        <Posibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
