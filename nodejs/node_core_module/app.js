3e79b0cf9429c0b6c8e32a8d2866bc7ab217e67e
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-04-08 09:34:03 +0700

    berhasil membuat command modifikasi kontak

--- nodejs/node_core_module/app.js
+++ nodejs/node_core_module/app.js
@@ -1,39 +1,79 @@
-const addContact = require('./addContact');
-const yargs = require('yargs');
+const methodContact = require("./methodContact");
+const yargs = require("yargs");
+
+yargs
+  .command({
+    command: "add",
+    describe: "Menambahkan kontak baru",
+    builder: {
+      nama: {
+        describe: "Nama Lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Email",
+        demandOption: false,
+        type: "string",
+      },
+      noHP: {
+        describe: "Nomor Handphone",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      methodContact.simpanContact(argv.nama, argv.email, argv.noHP);
+    },
+  })
+  .demandCommand(); // demandCommand() => fungsinya hanya untuk memberikan alert bila kita tidak menambahkan perintah atau parameter
 
 yargs.command({
-  command: 'add',
-  describe: 'Menambahkan kontak baru',
+  command: "list",
+  describe: "Menampilkan semua datar nama beserta nomor hp",
+  handler() {
+    methodContact.listContact();
+  },
+});
+
+yargs.command({
+  command: "detail",
+  describe: "Menampilkan satu contact berdasarkan nama",
   builder: {
     nama: {
       describe: "Nama Lengkap",
       demandOption: true,
-      type: 'string',
+      type: "string",
     },
-    email: {
-      describe: 'Email',
-      demandOption: false,
-      type: 'string',
-    },
-    noHP: {
-      describe: 'Nomor Handphone',
-      demandOption: true,
-      type: 'string',
-    }
   },
   handler(argv) {
-   addContact.simpanContact(argv.nama, argv.email, argv.noHP);
-  }
+    methodContact.detailContact(argv.nama);
+  },
+});
+
+yargs.command({
+  command: "delete",
+  describe: "Menghapus kontak berdasarkan nama",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    methodContact.deleteContact(argv.nama);
+  },
 });
 
 yargs.parse();
 
 /* async function main() {
-  const nama = await addContact.generalQuestion("Masukkan Nama mu : ");
-  const noHp = await addContact.generalQuestion("Masukkan No Telepon mu : ");
-  const email = await addContact.emailQuest();
+  const nama = await methodContact.generalQuestion("Masukkan Nama mu : ");
+  const noHp = await methodContact.generalQuestion("Masukkan No Telepon mu : ");
+  const email = await methodContact.emailQuest();
 
-  addContact.simpanContact(nama, noHp, email);
+  methodContact.simpanContact(nama, noHp, email);
 }
 
 main(); */
