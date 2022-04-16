75544a78cfb356ab5dc8542f72025fd4c7199482
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-04-16 21:48:06 +0700

    Belajar Basic dari typescript

---
+++ typescript/compile_typescript/intro.js
@@ -0,0 +1,10 @@
+/**
+ * Dilihat dari bahasa sendiri bahwa type scriopt sendiri merupakana bahasa program turunan dari Javascript yang dimana kita menuliskan syntaxnya dengan mencantumkan Tipe Data
+ */
+var character = "Ismail";
+// character = 1 akan terjadi error karena di awal kita sudah mendefiiniskan tipe data string jadi tidak bisa diubah
+var dinamicsNum = function (data) {
+    var calculate = data * Math.PI;
+    console.log(calculate);
+};
+dinamicsNum(10);
