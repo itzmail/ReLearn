a0011ee3d2a6b746e80c58f4fd5438bc851b5b73
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-04-17 16:26:16 +0700

    Membuat applikasi TodoList Basic Java sampai Menentukan Bussiness Logic

---
+++ Java/java_basic_todolist_app/src/ApplikasiTodoList.java
@@ -0,0 +1,33 @@
+public class ApplikasiTodoList {
+
+    // Kita buat Variabel yang nantinya digunakan untuk menampung semua todo berupa string
+    // Kita beri maksimal daya tampung sebanyak sepuluh, jika melebihi hal tersebut kita buat ulang lagi
+    public static String[] model = new String[10];
+
+    public static void main(String[] args){
+        System.out.println(model);
+    }
+
+    // Kita Buat Method Terlebih dahulu
+
+    /*
+    *  Menampilkan Todo ke List
+    */
+    public static void showTodoList(){
+
+    }
+
+    /*
+    * Menambahkan Todo ke list
+    */
+    public static void addTodoList(){
+
+    }
+
+    /*
+    * Mengahapus Todo dari list
+    */
+    public static void removeTodoList(){
+
+    }
+}
