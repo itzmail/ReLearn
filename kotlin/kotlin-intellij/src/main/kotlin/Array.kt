7e421909c0bd04cda2250e39cb07edf4d7e8302b
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-06-02 16:02:46 +0700

    Ganti JDK dan belajar ARray dan Nullable

---
+++ kotlin/kotlin-intellij/src/main/kotlin/Array.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val examArray = arrayOf(1, 2, true, "Mantap");
+    val charArray = charArrayOf('o', 'a', 'B'); // array elementnya char
+
+    val intArray = Array(4, {i -> i * i});
+
+    print(intArray[0]);
+    print(intArray[1]);
+    print(intArray[2]);
+    print(intArray[3]);
+}
