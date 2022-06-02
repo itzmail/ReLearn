acb2371004df626b2f363b9df94011e5608339ef
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-06-02 13:20:23 +0700

    Beljaar fungsi number dan string di kotlin

---
+++ kotlin/kotlin-intellij/src/main/kotlin/Function.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val user = setUser("Ismail", 20);
+    println(user);
+
+    printUser("Alfian");
+}
+
+fun setUser(name: String, age: Int): String{
+    return "Your name is $name and your age is $age";
+}
+
+fun printUser(name: String): Unit{
+    println("My name is $name");
+}
---
+++ kotlin/kotlin-intellij/src/main/kotlin/Number.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var max = Int.MAX_VALUE;
+    var min = Int.MIN_VALUE;
+//    println(max);
+//    println(min);
+
+    val byteNumber: Byte = 10;
+
+    val stringNumber = "23";
+    val intNumber = 3;
+
+    print(stringNumber.toInt() + intNumber);
+}
