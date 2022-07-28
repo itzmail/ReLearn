8123c5335ecf544dadcf037ca34b755c66029359
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-07-28 20:43:56 +0700

    Menambahkan keterangan

--- kotlin/kotlin-newbie/src/main/kotlin/OOP/3. PropertyDelegation.kt
+++ kotlin/kotlin-newbie/src/main/kotlin/OOP/3. PropertyDelegation.kt
@@ -2,6 +2,12 @@ package OOP
 
 import kotlin.reflect.KProperty
 
+/*
+* Pengelolaan properti kelas baik itu memberikan atau merubah sebuah nilai dapat didelegasikan kepada kelas lain. Dengan ini kita dapat meminimalisir boilerplate dalam penulisan getter dan setter (jika properties menggunakan var) pada setiap kelas yang kita buat. Sebagai contoh, kita memiliki tiga buah kelas yang di dalamnya memiliki satu properti String. Jika kita ingin menerapkan getter dan setter pada setiap properti kelasnya, maka kita perlu menuliskan getter dan setter tersebut pada seluruh kelas. Hal tersebut dapat mengurangi efisiensi dalam menuliskan kode karena terlalu banyak kode yang harus kita tulis secara berulang. Solusinya, kita perlu membuat sebuah kelas yang memang bertugas untuk mengatur atau mengelola fungsi getter dan setter untuk sebuah properti kelas. Teknik tersebut pada Kotlin dinamakan Delegate.
+
+Sebelum mendelegasikan sebuah properti kita perlu membuat kelas delegasi terlebih dahulu. Mari kita buat sebuah kelas delegasi.*/
+
+// Function dibawah termasuk dalam delegate Property
 class DelegateName {
     private var value: String = "Default"
 
