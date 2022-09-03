b5e6706355e83be7103f9947ad4f121f81412ac0
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-09-04 05:38:21 +0700

    Belajar Dart

---
+++ dart/first.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = "Ismail";
+  var year = 1921;
+  var anntenaDiameter = 3.7;
+  var flyByObjects = ["Jupiter", "Mars", "Bumi"];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print("Name : $name");
+  print(year);
+  print(flyByObjects);
+  print(image);
+  print(anntenaDiameter);
+}
