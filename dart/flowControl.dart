095b4417997f1f96f3f0117b7cd604a7b50b543d
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-09-05 14:40:57 +0700

    Belajar ternary

--- dart/flowControl.dart
+++ dart/flowControl.dart
@@ -1,5 +1,5 @@
 String ifElse(double data) {
-  print("If Check ${data}");
+  // print("If Check ${data}");
   if (data == 4.0) {
     return "A";
   } else if (data >= 2.0) {
@@ -10,7 +10,7 @@ String ifElse(double data) {
 }
 
 void switchCase(String data) {
-  print("Switch Case $data");
+  // print("Switch Case $data");
   switch (data) {
     case "A":
       print("Bukan Manusia");
@@ -23,8 +23,15 @@ void switchCase(String data) {
   }
 }
 
+String ternary(String data) {
+  // expr1 ?? expr 2 (non-null) expr2 dijalankan kalau kondisinya tidak null
+  return data == "A" || data == "B" ? "Anda lulus" : "Anda tidak lulus";
+}
+
 void main() {
-  double IPK = 0;
+  double IPK = 3.8;
 
   switchCase(ifElse(IPK));
+
+  print(ternary(ifElse(IPK)));
 }
