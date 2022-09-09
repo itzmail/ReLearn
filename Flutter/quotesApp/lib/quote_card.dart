afdea0f360dde1a606cf402ce21d3b6d3bd3039d
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-09-09 09:59:28 +0700

    Belajar Passing function as parram

--- Flutter/quotesApp/lib/quote_card.dart
+++ Flutter/quotesApp/lib/quote_card.dart
@@ -4,7 +4,8 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({required this.quote});
+  final Function delete;
+  QuoteCard({required this.quote, required this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +30,12 @@ class QuoteCard extends StatelessWidget {
                     fontSize: 14.0,
                     color: Colors.grey[800]
                 ),
-              )
+              ),
+              TextButton.icon(
+                  onPressed: () => delete(),
+                  icon: Icon(Icons.delete),
+                  label: Text("Delete Quote"),
+                  )
             ],
           ),
         )
