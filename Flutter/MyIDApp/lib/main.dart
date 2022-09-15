0b9bba02ccc78f839b6957647775ccee2dcfca51
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-09-16 05:55:32 +0700

    Review and Modif

--- Flutter/MyIDApp/lib/main.dart
+++ Flutter/MyIDApp/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: MyID()));
 
-class MyID extends StatefulWidget {
+class MyID extends StatefulWidget{
   const MyID({Key? key}) : super(key: key);
 
   @override
@@ -12,100 +10,104 @@ class MyID extends StatefulWidget {
 }
 
 class _MyIDState extends State<MyID> {
-
   int idLevel = 0;
+
+  void increLevel() {
+    setState(() {
+      idLevel += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text("My ID Card"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            idLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/dummyPP.jpg'),
-                radius: 50.0,
-              ),
-            ),
-            Divider(
-              height: 80.0,
-              color: Colors.grey[800]
-            ),
-            Text(
-              "Name".toUpperCase(),
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "Ismail",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              "CURRENT NINJA LEVEL".toUpperCase(),
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "$idLevel",
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'ismailnuralam@imaco.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0
-                  )
-                )
-              ],
-            ),
-          ],
-        )
-      ),
-    );
+   return Scaffold(
+     backgroundColor: Colors.grey[900],
+     appBar: AppBar(
+       title: Text("Flutter Ground"),
+       centerTitle: true,
+       automaticallyImplyLeading: false,
+       backgroundColor: Colors.grey[850],
+       elevation: 0.0,
+     ),
+     body: Padding(
+       padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Center(
+             child: CircleAvatar(
+               backgroundImage: AssetImage('assets/dummyPP.jpg'),
+               radius: 50.0,
+             ),
+           ),
+           Divider(
+             height: 80.0,
+             color: Colors.grey[800],
+           ),
+           Text(
+             "NAME",
+             style: TextStyle(
+                 color: Colors.grey,
+                 letterSpacing: 2.0
+             ),
+           ),
+          SizedBox(height: 10.0),
+           Text(
+             "Ismail Nur Alam",
+             style: TextStyle(
+                 color: Colors.amberAccent[200],
+                 letterSpacing: 2.0,
+                 fontSize: 25.0,
+                 fontWeight: FontWeight.bold
+             ),
+           ),
+           SizedBox(height: 20.0),
+           Text(
+             "CURRENT NINJA LEVEL",
+             style: TextStyle(
+               color: Colors.grey,
+               letterSpacing: 2.0
+             ),
+           ),
+          SizedBox(height: 10.0),
+           Text(
+             "$idLevel",
+             style: TextStyle(
+               color: Colors.amberAccent[200],
+               letterSpacing: 2.0,
+               fontSize: 25.0,
+               fontWeight: FontWeight.bold
+             ),
+           ),
+           SizedBox(height: 20.0),
+           Text(
+             "EMAIL",
+             style: TextStyle(
+               color: Colors.grey,
+               letterSpacing: 2.0
+             ),
+           ),
+          SizedBox(height: 10.0),
+           Text(
+             "ismail@imaco.com",
+             style: TextStyle(
+               color: Colors.amberAccent[200],
+               letterSpacing: 2.0,
+               fontSize: 25.0,
+               fontWeight: FontWeight.bold
+             ),
+           ),
+         ],
+       ),
+     ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: () => increLevel(),
+       child: Icon(Icons.add),
+       backgroundColor: Colors.grey[850],
+       hoverColor: Colors.indigo,
+       tooltip: "Tambah level mu",
+     ),
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+   );
   }
 }
-
