557f83fb20cf555910b2dee1662aa57a72973742
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-09-15 15:55:42 +0700

    Make Branch nodejs

--- Flutter/quotesApp/android/app/src/main/kotlin/com/itsmail/quotes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.itsmail.quotes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter/world_time/android/app/src/main/kotlin/com/itsmail/world_time/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.itsmail.world_time
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
