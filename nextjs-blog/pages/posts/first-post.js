b6408fe75916357196b14c9bdebdedf5c15e14ac
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-10-13 15:42:35 +0700

    Study with image

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,24 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
       </h2>
-    </>
+      <Image
+        src={"/images/profile.jpg"}
+        height={144}
+        width={144}
+        alt="Dummy Image"
+      />
+    </div>
   )
 }
