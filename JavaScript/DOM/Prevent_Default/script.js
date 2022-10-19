27700345a45e2131a2b013a6ea2d4a2f4debd6ac
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-10-19 11:29:32 +0700

    Make new branch golang

--- JavaScript/DOM/Prevent_Default/script.js
+++
@@ -1,9 +0,0 @@
-// Event Handling
-const closes = document.querySelectorAll('.close');
-
-closes.forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.target.parentElement.style.display = 'none';
-    e.preventDefault(); // berguna untuk mencegah aksi default dari komputer dan menggunakan method dari kita sendiri
-  });
-});
