d70c80bcf7f986d2322f6c09c20ecc07da04a5b2
Ismail N.A <77151067+itzmail@users.noreply.github.com>
2023-01-01 17:39:59 +0700

    Belajar if else and swithc

---
+++ study/switch.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  name := "Ismail"
+
+  switch name {
+  case "Mantap":
+    fmt.Println("Hello Mantap")
+  case "Ismail":
+    fmt.Println("Hello Ismail") 
+  default : 
+    fmt.Println("Nama undetected")
+  } 
+
+  // Short Statemen 
+  // switch length := len(name); length > 5 {
+  //  case true:
+  //    fmt.Println("Nama Terlalu Panjang")
+  //  case false:
+  //    fmt.Println("Nama Sudah Benar")
+  // }
+
+  length := len(name)
+  switch {
+  case length > 5 :
+    fmt.Println("Nama terlalu panjang")
+  case length < 5 :
+    fmt.Println("Nama Mas juang kampungan")
+  }
+
+  // Switch tanpa kondisi
+}
