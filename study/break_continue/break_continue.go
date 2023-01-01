b6061be58e655141fee573805e0b5eb78c6762d4
Ismail N.A <77151067+itzmail@users.noreply.github.com>
2023-01-01 21:15:09 +0700

    Belajar break and continue

--- study/break_continue/break_continue.go
+++ study/break_continue/break_continue.go
@@ -8,6 +8,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 		if i == 5 {
 			break
+			// perulanngan saat i == 5 maka akan berhenti
 		}
 
 		fmt.Println("Perulangan ke-", i)
@@ -16,6 +17,7 @@ func main() {
 	for i := 10; i >= 0; i-- {
 		if i%2 == 0 {
 			continue
+			// kode di bawah akan diskip
 		}
 
 		fmt.Println("Perulangan ke-", i) // Print yang ganjil
