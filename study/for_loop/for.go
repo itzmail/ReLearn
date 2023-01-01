01770577a401419108df9b492aa5042d00fd132f
Ismail N.A <77151067+itzmail@users.noreply.github.com>
2023-01-01 21:06:31 +0700

    Belajar for loop

--- study/for_loop/for.go
+++ study/for_loop/for.go
@@ -17,8 +17,33 @@ func main() {
 	}
 
 	// Bentuk Ringkas
-
 	for countdown := 10; countdown >= 0; countdown-- {
 		fmt.Println("Hitung mundur", countdown)
+	}
+
+	/* FOR RANGE */
+	iniSlice := []any{"Mantap", "Jiwa", 12}
+
+	// 1
+	for countSlice := 0; countSlice < len(iniSlice); countSlice++ {
+		fmt.Println("data : ", iniSlice[countSlice])
+	}
+
+	// 2 ( pakai ini kalau pakai kayak foreach)
+	// for index, data := range iniSlice {
+	// fmt.Println("Index ", index, "=", data)
+	// kalau kita ngga mau pakai index kasih _, kita kasih tahu golang kalau kita ngga pakai variable itu
+	for _, data := range iniSlice {
+		fmt.Println(data)
+	}
+
+	// Untuk perulangan Map
+	iniMap := make(map[string]string)
+
+	iniMap["name"] = "Ismail"
+	iniMap["title"] = "Direktur"
+
+	for kunci, value := range iniMap {
+		fmt.Print(kunci, "=", value)
 	}
 }
