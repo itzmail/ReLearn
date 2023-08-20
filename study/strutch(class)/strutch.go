dd45bbf629183e074fdf6e0e8f50b44b9fbd4d11
Ismail N.A <77151067+itzmail@users.noreply.github.com>
2023-08-20 20:20:40 +0700

    belajar Strutch

--- study/strutch(class)/strutch.go
+++ study/strutch(class)/strutch.go
@@ -1,25 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 /**
   struct dipakai untuk merepresentasikan data,
   Struct sendiri bisa didefinisikan sebagai kumpulan beberapa field (object)
-  
+
   kalau di OOP namanya class
 */
 
 type Customer struct {
-  name, address string;
-  age int
+	name, address string
+	age           int
 }
 
 func main() {
-  ismail := Customer {
-    name: "Ismail",
-    address: "Magelang",
-    age: 22,
-  }
+	ismail := Customer{
+		name:    "Ismail",
+		address: "Magelang",
+		age:     22,
+	}
 
-  fmt.Println(ismail)
+	fmt.Println(reflect.TypeOf(ismail))
 }
