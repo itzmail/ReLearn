dee84b684d250e3312a74a486afe37ffcfc79191
itzmail <ismailnuralam@gmail.com>
2023-11-27 21:49:33 +0700

    Learn Formater

---
+++ formater/calculator.py
@@ -0,0 +1,10 @@
+class Calculator:
+    """Kalkulator tambah dan kurang"""
+    def __init__(self, _i):
+        self.i = _i
+
+    def tambah(self, _i):
+        return self.i + _i
+
+    def kurang(self, _i):
+        return self.i - _i
