f38f7b3e62e4650c6f4e717e23996aaf923fb0b5
Ismail N.A <77151067+itzmail@users.noreply.github.com>
2024-02-04 10:37:49 +0700

    preprare file

---
+++ bloc/crud/lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+    const User({
+        required this.name,
+        required this.id,
+        required this.email
+    })
+
+    final Int id;
+    final String name;
+    final String email;
+}
