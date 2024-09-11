de7057e74cf50a437144d0a75e1ab1352c73f9bc
ismailalam <ismailnuralam@gmail.com>
2024-09-11 22:38:48 +0700

    try graphql

--- graphql/src/main/java/com/learn/graphql/GraphqlApplication.java
+++ graphql/src/main/java/com/learn/graphql/GraphqlApplication.java
@@ -1,7 +1,19 @@
 package com.learn.graphql;
 
+ import java.util.Collection;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import org.springframework.stereotype.Service;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.TypeRuntimeWiring;
 
 @SpringBootApplication
 public class GraphqlApplication {
@@ -10,4 +22,35 @@ public class GraphqlApplication {
 		SpringApplication.run(GraphqlApplication.class, args);
 	}
 
+	@Bean
+	RuntimeWiringConfigurer runtimeWiringConfigurer() {
+		return new RuntimeWiringConfigurer() {
+			@Override
+			public void configure(RuntimeWiring.Builder builder) {
+				builder.type("Query", new UnaryOperator<TypeRuntimeWiring.Builder>() {
+					@Override
+					public TypeRuntimeWiring.Builder apply(TypeRuntimeWiring.Builder wiring) {
+						return wiring
+								.dataFetcher("customers", new DataFetcher<>() {
+									@Override
+									public Object get(DataFetchingEnvironment environemnt) throws Exception {
+										return null;
+									}
+								});
+					}
+				});
+			}
+		};
+	}
+}
+
+record Customer (Integer id, String name) {}
+@Service
+class CrmService {
+	Collection <Customer> getCustomers() {
+		 return List.of(
+			new Customer(1, "Jhond"),
+			new Customer(2, "Doe")
+		 );
+	}
 }
